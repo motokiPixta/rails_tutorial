9bd30a078a8a9785d2442454c7d42852690b8821
motoki_saito <motoki_saito@pixta-no-MacBook-Air.local>
2016-06-13 12:25:56 +0900

    途中

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 						password_confirmation: "bar" }
 		end
 		assert_template "users/new"
+		assert_select 'div#error_explanation'
+		assert_select 'div[class="alert alert-danger"]'
 	end
 
 	test "valid signup information" do
@@ -22,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			password_confirmation: "password"}
 		end
 		assert_template "users/show"
+		assert_not flash.nil?
 	end
 						
 end
