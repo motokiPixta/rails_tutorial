cb84af8fc2ab1873e803769643663be2236f91a7
motoki_saito <motoki_saito@pixta-no-MacBook-Air.local>
2016-08-31 10:57:02 +0900

    Finish log in/log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+	  p params
 	  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,13 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
+  def forget(user)
+    user.forget
+	cookies.delete(:user_id)
+	cookies.delete(:remember_token)
+  end
+ 
   def current_user 
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
@@ -26,11 +32,7 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-  def forget(user)
-    user.forget
-	cookies.delete(:user_id)
-	cookies.delete(:remember_token)
-  end
+  
   
   def log_out
     forget(current_user)
