30404bdf9409bbdf2b8e611bd2031ba30df1d558
motoki_saito <motoki_saito@pixta-no-MacBook-Air.local>
2016-08-31 11:49:45 +0900

    9.1.1

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -36,7 +36,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not_nil cookies['remember_token']
+	p assigns(:user)
+	assert_not_nil cookies['remember_token']
+    assert assigns(:user).authenticated?(cookies['remember_token'])
   end
  
   test "login without remembering" do
