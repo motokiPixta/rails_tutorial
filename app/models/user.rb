5623b7d06abb01e13f4a0a1cdb41c07f516f7e79
motoki_saito <motoki_saito@pixta-no-MacBook-Air.local>
2016-09-02 18:28:17 +0900

    Add password resets & email confirguration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true , length: { maximum: 50 }
@@ -26,8 +26,8 @@ class User < ActiveRecord::Base
 
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
-	return false if digest.nil?
-	BCrypt::Password.new(digest).is_password?(token)
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
@@ -36,11 +36,25 @@ class User < ActiveRecord::Base
 
   def activate
     update_attribute(:activated, true)
-	update_attribute(:activated_at, Time.zone.now)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   private 
   def downcase_email
@@ -49,6 +63,6 @@ class User < ActiveRecord::Base
 
   def create_activation_digest
     self.activation_token = User.new_token
-	self.activation_digest = User.digest(activation_token)
+    self.activation_digest = User.digest(activation_token)
   end
 end
